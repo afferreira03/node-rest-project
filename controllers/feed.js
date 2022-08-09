9909cd7a1a4c816a8e24d2e4bbeee6e8fb75ca84
Alan Felipe Ferreira <alan.fferreira03@gmail.com>
2022-08-09 09:31:38 -0300

    chore: adição de controllers e feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,33 +1,92 @@
-exports.getPosts = (req, res, next) =>{
-    res.status(200).json({
-        posts: [
-            {
-                _id: '1',
-                title: 'This is the first post!',
-                content: 'This is the first post of this blog',
-                imageUrl: 'images/token_allain.png',
-                creator: {
-                    name: 'Dummy Author'
-                },
-                createdAt: new Date()
+const Post = require('../models/post');
+const { validationResult } = require('express-validator');
+
+exports.getPosts = (req, res, next) => {
+    Post.find()
+        .then(posts => {
+            if (!posts) {
+                const error = new Error('Post doesn\'t  exist.');
+                error.statusCode = 404;
+                throw error;
             }
-        ]
-    });
+
+            res.status(200)
+                .json({
+                    posts: posts
+                });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 }
 
 exports.createPost = (req, res, next) => {
-    const title = req.body.title; 
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validations failed, entered data is incorrect.');
+        error.status = 422;
+        throw error;
+    }
+
+    if(!req.file) {
+        const error = new Error('No image provided');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const imageUrl = req.file.path.replace('\\', '/');
+    const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'Post created.',
-        post: {
-            _id : new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Alan',
-            },
-            createdAt: new Date()
+        const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: imageUrl,
+        creator: {
+            name: 'Alan',
         }
-    })
+    });
+
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201)
+                .json({
+                    message: 'Post created.',
+                    post: result
+                });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Post doesn\'t  exist.');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            res.status(200).json({
+                message: 'post fetch',
+                post: post
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err)
+        });
 }
